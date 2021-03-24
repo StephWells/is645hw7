6ebf98e47c035faa38ec5f8b388da7212fd45383
Stephanie Wells <sw.stephwells@gmail.com>
2021-03-23 20:09:32 -0700

    DropDown Test

--- js/ex1.js
+++ js/ex1.js
@@ -17,54 +17,11 @@ const houses = [{
 }
 ];
 
-// Return an array of characters belonging to a house
-const getCharacters = houseCode => {
-switch (houseCode) {
-    case "ST":
-        return ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"];
-    case "LA":
-        return ["Tywin", "Cersei", "Jaime", "Tyrion"];
-    case "BA":
-        return ["Robert", "Stannis", "Renly"];
-    case "TA":
-        return ["Aerys", "Daenerys", "Viserys"];
-    default:
-        return []; //Empty array
-}
-};
+const houseDDL = document.getElementById("house");
 
-const option = (text, value) => {
-const elementA = document.createElement("option");
-elementA.textContent = text;
-elementA.value = value;
-return elementA;
-};
-
-
-const list = houselist => {
-const elementB = document.list("li");
-elementB.textContent = houselist;
-return elementB;
-};
-
-
-const housElement = document.querySelector("select");
 houses.forEach(house => {
-housElem.appendChild(option(house.name, house.code));
-});
-
-
-housElem.addEventListener("change", e => {
-
-const char = getCharacters(e.target.value);
-const charElement = document.getElementById("characters");
-
-
-
-charElement.innerHTML = "";
-
-
-char.forEach(character => {
-    charElement.appendChild(list(character));
-});
+    const houseElement = document.createElement("option");
+    houseElement.value = house.code;
+    houseElement.textContent = house.name;
+    houseDDL.appendChild(houseElement);
 });
